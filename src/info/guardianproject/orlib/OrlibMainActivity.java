ae7f53ed7f462e12e8beb2ba6bf244b56d8dbaaf
The Guardian Project <root@guardianproject.info>
2010-06-24 14:29:04 -0400

    Adding in basic empty project files for Orlib Onion Routing Library

---
+++ src/info/guardianproject/orlib/OrlibMainActivity.java
@@ -0,0 +1,13 @@
+package info.guardianproject.orlib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrlibMainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
