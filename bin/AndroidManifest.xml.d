98cef984d3e7fef899d649885abd7bfc0ea5b37d
Artem Smirnov <a.smirnov@labster.pro>
2014-01-11 20:23:18 +0400

    Fixed command-line build

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/rankor/src/3rd-party/android/Orlib/bin/AndroidManifest.xml : \
-/Users/rankor/src/3rd-party/android/Orlib/AndroidManifest.xml \
--- bin/Orlib.ap_.d
+++
@@ -1,6 +0,0 @@
-/Users/rankor/src/3rd-party/android/Orlib/bin/Orlib.ap_ \
- : /Users/rankor/src/3rd-party/android/Orlib/res/drawable/icon.png \
-/Users/rankor/src/3rd-party/android/Orlib/res/layout/main.xml \
-/Users/rankor/src/3rd-party/android/Orlib/res/values/strings.xml \
-/Users/rankor/src/3rd-party/android/Orlib/bin/res/drawable/icon.png \
-/Users/rankor/src/3rd-party/android/Orlib/bin/AndroidManifest.xml \
