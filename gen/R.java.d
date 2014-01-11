daee5fdc1340e1c77819baccd3ddbd14bacf98f2
Artem Smirnov <a.smirnov@labster.pro>
2014-01-11 20:23:52 +0400

    Fixed command-line build

--- gen/R.java.d
+++
@@ -1,5 +0,0 @@
-/Users/rankor/src/3rd-party/android/Orlib/gen/info/guardianproject/orlib/R.java \
- : /Users/rankor/src/3rd-party/android/Orlib/res/drawable/icon.png \
-/Users/rankor/src/3rd-party/android/Orlib/res/layout/main.xml \
-/Users/rankor/src/3rd-party/android/Orlib/res/values/strings.xml \
-/Users/rankor/src/3rd-party/android/Orlib/bin/AndroidManifest.xml \
